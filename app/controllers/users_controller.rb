3aaab766967ef866191d65ea0fedd5642c422b83
vagrant <vagrant@localhost.localdomain>
2020-06-27 15:59:59 +0000

    Review Bookers2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :correct_user, only:[:edit, :update]
+
+  def index
+  	@users = User.all
+  	@book = Book.new
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  	@book = Book.new
+  end
+
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	if @user.save
+  		redirect_to user_path(@user), notice: 'You have updated user successfully.'
+  	else
+  		render :edit
+  	end
+  end
+
+  def correct_user
+  	@user = User.find(params[:id])
+  	unless @user == current_user
+  		redirect_to user_path(current_user)
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'dfb141841308f8484b91d4fb59670217d7bd919248260fd316253b4bb98c5b4306b1e597883ffac4575d943fa10477b3ed0af752583a558a8778459e3866c27f'
